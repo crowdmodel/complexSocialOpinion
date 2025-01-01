5123ca04974fc5d11f2b344174593c947e4375f8
godisreal <wp2204@gmail.com>
2025-01-01 19:59:51 +0800

    Add email address.

--- src/flow.py
+++ src/flow.py
@@ -11,6 +11,11 @@
 # This software is a python library for Many-Particle Simulation of Complex Social Interaction
 # The individual-level model is extended based on the well-known social force model, and it mainly describes how agents/particles interact with each other, and also with surrounding facilities including obstructions and passageways. Most importantly, we introduce a set of arrays to define social relationship of agents/particles in a quantitative manner. Opinion dynamics is integrated with force-based interaction to study complex social phenonmena including path-selection activities, social group and herding effect.  Verying interestingly, the interaction of such agent/particles are not only at physics-level, but at consciousness and unconsciousness level by integratings advance social-psychological studies.  
 
+
+# -*-coding:utf-8-*-
+# Author: WP
+# Email: wp2204@gmail.com
+
 import os, sys
 import numpy as np
 #import matplotlib.pyplot as plt
